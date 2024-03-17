bd667cf0205512ef0ef5de2a53471f240fff3151
Do Hyeon Seok <dohyeondol@gmail.com>
2024-03-18 05:06:46 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 172 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int n;
+int code1 = 0, code2 = 0;
+
+int fib(int n) {
+    if (n == 1 || n == 2) code1 ++;
+    else{
+        return (fib(n - 1) + fib(n - 2));
+    }
+}
+
+int main(){
+    int f[41] = {1, 1,};
+    scanf("%d", &n);
+    
+    fib(n);
+    for(int i = 3; i <= n; i++){
+        f[i] = f[i-1] + f[i-2];
+        code2 ++;
+    }
+    
+    printf("%d %d", code1, code2);
+}
